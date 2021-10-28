b4a651c374b72b8fcf81eb87b2a69f1cf9b2cbd8
Aaliya <shamsaaliya08@gmail.com>
2021-10-28 13:35:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [user,setUser] = useState('');
+  const handlechange = (event) =>{
+    setUser(event.target.value);
+    console.log(user);
+  }
+
+  const handlesubmit = () =>{
+      console.log(user);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>CometCall</h1>
+     <input 
+     type='text'
+     value={user} 
+     onChange={handlechange}/>
+     
+     <button
+     style={{width:'100px', height:'40px'}} 
+     onClick={handlesubmit} >
+      Button
+    </button>
     </div>
   );
 }
